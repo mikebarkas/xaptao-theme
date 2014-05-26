10832fc1c50f0092342e0fc135f25cdfee1ddce9
Mike Barkas <mike@barkas.com>
2014-05-26 11:22:19 -0400

    Started Enquire.

---
+++ js/layout.js
@@ -0,0 +1,14 @@
+enquire.register("screen and (min-width: 900px)", {
+
+	deferSetup: true,
+
+	  setup : function() {
+	    // console.log("Setup Desktop matched");
+	  },
+	  match : function() {
+	  	// console.log("Handler Desktop matched");
+	  },
+	  unmatch : function() {
+	  	// console.log("Handler Mobile matched");
+	  }
+});
