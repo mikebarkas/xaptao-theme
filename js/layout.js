31d57da5bd83e2ac32c6890c0f76458c2f3c7526
Mike Barkas <mike@barkas.com>
2014-05-28 20:50:30 -0400

    Sass files.

--- js/layout.js
+++ js/layout.js
@@ -2,13 +2,13 @@ enquire.register("screen and (min-width: 900px)", {
 
 	deferSetup: true,
 
-	  setup : function() {
-	    // console.log("Setup Desktop matched");
-	  },
-	  match : function() {
-	  	// console.log("Handler Desktop matched");
-	  },
-	  unmatch : function() {
-	  	// console.log("Handler Mobile matched");
-	  }
+  setup : function() {
+    // console.log("Setup Desktop matched");
+  },
+  match : function() {
+  	// console.log("Handler Desktop matched");
+  },
+  unmatch : function() {
+  	// console.log("Handler Mobile matched");
+  }
 });
